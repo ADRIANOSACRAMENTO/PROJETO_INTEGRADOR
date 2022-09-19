ee197721ffba8677346eb47d94ad06dd0a3af777
ADRIANOSACRAMENTO <adriano.ssacramento@hotmail.com>
2022-09-19 18:17:07 -0300

    Criacao dos Frames

--- main.py
+++ main.py
@@ -16,15 +16,24 @@ co7 = "#3fbfb9" #Verde 1
 co8 = "#263738" #Verde +
 co9 = "#e9edf5" #Cinza
 
-
 #criando janela em branco
 janela = Tk ()
 janela.title("")
 janela.geometry('900x600')
 janela.configure(background=co9)
-janela.resizable(width=FALSE, height=FALSE)
 
+#janela.resizable(width=FALSE, height=FALSE)
 style = ttk.Style(janela)
 style.theme_use("clam")
+
+#criacao dos frames
+frameCima = Frame(janela, width=1050, height=50, background=co1, relief="flat")
+frameCima.grid(row=0, column=0 )
+
+frameMeio = Frame(janela, width=1050, height=303, bg=co1, pady=20, relief="flat")
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW )
+
+frameBaixo = Frame(janela, width=1050, height=300, bg=col, relief="sunken")
+frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW )
 
 janela.mainloop()
